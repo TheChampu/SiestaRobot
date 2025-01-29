f11cd2ff41bd1b93d3aa29f1defaef35395bc571
Champu <117635421+TheChampu@users.noreply.github.com>
2025-01-29 20:44:48 +0530

    Update mongo.py

--- SiestaRobot/mongo.py
+++ SiestaRobot/mongo.py
@@ -7,14 +7,14 @@ from SiestaRobot import MONGO_DB_URI
 from SiestaRobot.confing import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27017")
+
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "SiestaRobot"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+client = MongoClient(MONGO_DB_URI)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
 db = motor[MONGO_DB]
 db = client["VainBot"]
 try:
