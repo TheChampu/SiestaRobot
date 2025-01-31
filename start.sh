878e9777b7e7157bc130bd49b93cb1eb2d7ef6c3
Champu <117635421+TheChampu@users.noreply.github.com>
2025-01-31 23:20:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo "Starting application..."
-# Ensure time synchronization
-current_time=$(curl -s --head http://worldtimeapi.org/api/timezone/Etc/UTC | grep ^Date: | sed 's/Date: //g')
-date -u -s "$current_time"
-sleep 5  # Wait for time sync
-echo "Time synchronized: $(date)"
 # Start the application
 python SiestaRobot/__main__.py
