e5475f8afd7dfb86744cec8a8d0559f7f39034b0
Champu <117635421+TheChampu@users.noreply.github.com>
2025-01-31 23:09:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting application..."
 # Ensure time synchronization
-sudo ntpdate ntp.ubuntu.com
+ntpdate ntp.ubuntu.com
 sleep 5  # Wait for time sync
 echo "Time synchronized: $(date)"
 # Start the application
